ad955a713d3776f0478b59b252835c732ebbb473
Dr Nic Williams <drnicwilliams@gmail.com>
2015-05-07 16:22:08 -0700

    spiffy merges in a meta scope

--- lib/makemespiffy/input_manifest.rb
+++ lib/makemespiffy/input_manifest.rb
@@ -12,5 +12,31 @@ module MakeMeSpiffy
     def initialize(bosh_manifest_yaml)
       @manifest = bosh_manifest_yaml
     end
+
+    # Primary method to replace a chunk of manifest with a (( meta.scope ))
+    def spiffy(extraction_scope, meta_scope)
+      if value = manifest[extraction_scope]
+        manifest[extraction_scope] = "(( #{meta_scope} ))"
+        insert_meta_scope(meta_scope, value)
+      end
+      return value
+    end
+
+    private
+    # Usage: insert_meta_scope("meta.foo.bar", 1234)
+    # Will add the following into manifest YAML
+    #     meta:
+    #       foo:
+    #         bar: 1234
+    def insert_meta_scope(meta_scope, value)
+      parts = meta_scope.split('.')
+      submanifest = manifest
+      scoping_parts, key = parts[0..-2], parts[-1]
+      for part in scoping_parts
+        submanifest[part] ||= {}
+        submanifest = submanifest[part]
+      end
+      submanifest[key] = value
+    end
   end
 end
--- spec/input_manifest_spec.rb
+++ spec/input_manifest_spec.rb
@@ -2,6 +2,17 @@ describe MakeMeSpiffy::InputManifest do
   describe 'concourse.yml' do
     let(:fixture) { fixture_path("concourse.yml") }
     subject { MakeMeSpiffy::InputManifest.from_file(fixture) }
-    it { expect(subject.manifest["name"]).to eq "concourse" }
+
+    describe ".name" do
+      it { expect(subject.manifest["name"]).to eq "concourse" }
+      describe "spiffy" do
+        before do
+          @value = subject.spiffy("name", "meta.name")
+        end
+        it {expect(subject.manifest["name"]).to eq "(( meta.name ))"}
+        it {expect(subject.manifest["meta"]).to eq({"name" => "concourse"}) }
+        it {expect(@value).to eq "concourse"}
+      end
+    end
   end
 end
